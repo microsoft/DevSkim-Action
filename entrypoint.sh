acd37ac4cc2b6011a5a87fe74ee4700e6cbac1b1
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-07-14 19:48:11 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,14 @@
-echo "Positional Parameters"
-echo '$0 = ' $0
-echo '$1 = ' $1
-echo '$2 = ' $2
-echo '$3 = ' $3
+# $1 is the directory to scan
+# $2 is if we should crawl archives
+# $3 is the output filename
 
-if [ "$1" == "GITHUB_WORKSPACE" ]
-    $Directory = $GITHUB_WORKSPACE
+if [ "$1" = "GITHUB_WORKSPACE" ]; then
+    Directory=$GITHUB_WORKSPACE
 else
-    $Directory = $1
+    Directory=$1
 fi
 
-if [ "$2" == "true" ]
+if [ "$2" = "true" ]; then
     devskim analyze $Directory -f sarif -c > $GITHUB_WORKSPACE/$3
 else
     devskim analyze $Directory -f sarif > $GITHUB_WORKSPACE/$3
