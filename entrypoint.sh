430c605a9149e1c8f2d5ced90ec469e6ccdd858e
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-10-25 22:02:48 -0700

    Use the bath path feature to root results to the github workspace even when a subpath is specified.

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,4 +22,4 @@ if [ "$2" = "true" ]; then
     Opts = "-c"
 fi
 
-/tools/devskim analyze "$ScanTarget" "$OutputDirectory/$3" -f sarif $Opts -g $5
+/tools/devskim analyze "$ScanTarget" "$OutputDirectory/$3" -f sarif $Opts -g $5 --base-path $GITHUB_WORKSPACE
